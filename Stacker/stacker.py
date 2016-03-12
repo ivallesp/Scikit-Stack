7bfeef83b496b908413009f581769813a325dbea
ivallesp <ivanvallesperez@gmail.com>
2016-03-12 02:00:08 +0100

    Add docstrings for the Stacker methods, Moved the "model" parameter from the Stacker init method to the generate_training_metapredictor method. Add statements for storing the last predictors calculated (will be used in the future for the methods that store the predictors). Add tests for looking for inconsistencies Add tests for checking if the predictors are saved as atributes in the Stacker class Add method for storing predictor files Add tests for the save_files() method

--- Stacker/stacker.py
+++ Stacker/stacker.py
@@ -1,4 +1,6 @@
 __author__ = 'ivanvallesperez'
+import os
+
 import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score
@@ -7,15 +9,38 @@ from Stacker.data_operations import CrossPartitioner
 
 
 class Stacker():
-    def __init__(self, train_X, train_y, train_id, model, folds=10, stratify=True):
+    def __init__(self, train_X, train_y, train_id, folds=10, stratify=True):
+        """
+        This class is the main class of the Stacker. Its main purpose is to properly generate the stacked prediction
+        assuring that the indices of the predictions are aligned.
+        :param train_X: the input data for training (numpy.ndarray, scipy.sparse.csr, pandas.Dataframe)
+        :param train_y: target for the training data (list, numpy.ndarray, pandas.Dataframe)
+        :param train_id: id for the training data (list, numpy.ndarray, pandas.Dataframe). Used for assuring that the
+        indices are properly aligned with the original data
+        :param folds: Number of folds (int, default=10)
+        :param stratify: Whether to preserve the percentage of samples of each class (boolean, default=False)
+        :return: None
+        """
         self.train_X = train_X
         self.train_y = train_y
         self.train_id = train_id
-        self.model = model
+        self.model = None
         self.folds = folds
         self.stratify = stratify
 
-    def generate_training_metapredictor(self):  # TODO: Idea, move here the parameter "model"
+    def generate_training_metapredictor(self, model):
+        """
+        This function is responsible for iterating across a CV loop with a specific (hard-coded) random seed, training
+        the inner models and generating the stacked predictor (aligned with the training set).
+        :param model: sklearn-like instanced model. It is requiered that it contains the 'fit' and 'predict_proba'
+         methods (class).
+        :return: pd.DataFrame with only one column containing the target. The ID of this DataFrame has to be aligned
+        (i.e. to be the same) as the original training set index (pandas.Dataframe)
+        """
+        self.training_predictor = None
+        self.test_predictor = None
+        self.model = None
+        self.model = model
         cp = CrossPartitioner(n=len(self.train_y) if not self.stratify else None,
                               y=self.train_y,
                               k=self.folds,
@@ -43,13 +68,26 @@ class Stacker():
         self.cv_score_std = np.std(scores)
         training_predictor = pd.DataFrame({"target": prediction_batches}, index=indices_batches).ix[self.train_id]
         assert len(training_predictor) == len(self.train_X)
+        self.training_predictor = training_predictor
         return training_predictor
 
     def generate_test_metapredictor(self, test_X, test_id):
+        self.test_predictor = None
         self.model.fit(self.train_X, self.train_y)
         test_prediction = self.model.predict_proba(test_X)
         test_prediction = np.reshape(test_prediction, (len(test_id), test_prediction.ndim))  # this code
         # forces having 2D
         test_prediction = test_prediction[:, -1]  # Extract the last column
         test_predictor = pd.DataFrame({"target": test_prediction}, index=test_id)
+        self.test_predictor = test_predictor
         return test_predictor
+
+    def save_files(self, alias=None, folder="/tmp"):
+        if not alias:
+            training_filePath = os.path.join(folder, "training_metapredictor.csv")
+            test_filePath = os.path.join(folder, "test_metapredictor.csv")
+        else:
+            training_filePath = os.path.join(folder, alias + "_training_metapredictor.csv")
+            test_filePath = os.path.join(folder, alias + "_test_metapredictor.csv")
+        self.training_predictor.to_csv(training_filePath, sep=",", encoding="utf-8", index_label="id")
+        self.test_predictor.to_csv(test_filePath, sep=",", encoding="utf-8", index_label="id")
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ivanvallesperez'
+
 import unittest
 
+import scipy as sp
 from sklearn.datasets import *
 
 from Stacker.stacker import *
@@ -18,7 +20,6 @@ class TestBasic(unittest.TestCase):
         data = [data[0][index_randperm, :], data[1][index_randperm]]
         out_of_sample = [data[0][1000:2000, :], data[1][1000:2000]]
         data = [data[0][0:1000, :], data[1][0:1000]]
-
         test_X, test_y = out_of_sample[0], out_of_sample[1]
         X, y = data[0], data[1]
         id = np.random.permutation(range(len(y)))  # We random permutate it to make the problem harder.
@@ -27,9 +28,8 @@ class TestBasic(unittest.TestCase):
         self.assertEqual(len(X), len(y),
                          "Error loading hastie data with sklearn. Train/test data with different sizes.")
 
-
-        s = Stacker(train_X=X, train_y=y, train_id=id, model=model, stratify=False)
-        y_hat_training = s.generate_training_metapredictor()
+        s = Stacker(train_X=X, train_y=y, train_id=id, stratify=False)
+        y_hat_training = s.generate_training_metapredictor(model=model)
         self.assertIn("cv_score_mean", dir(s), "Atribute 'cv_score_mean' not found in the Stacker object")
         self.assertIn("cv_score_std", dir(s), "Atribute 'cv_score_std' not found in the Stacker object")
         self.assertAlmostEqual(s.cv_score_mean, 0.9, delta=0.05, msg="Score value given by the model ('%s' ± '%s') "
@@ -38,12 +38,116 @@ class TestBasic(unittest.TestCase):
         # Differences produced basically because the size of the sample when calculating the AUC gives the resolution of
         # the curve. As the resolution changes, the result also slightly changes. We are calculating the roc with
         # different sample sizes.
+        self.assertIn("training_predictor", dir(s), "Atribute 'training_predictor' not found in the Stacker object")
+        self.assertTrue(type(s.training_predictor) != None)
 
         y_hat_test = s.generate_test_metapredictor(test_X, test_id)
-        test_score = roc_auc_score(test_y, y_hat_test)
+        self.assertAlmostEqual(roc_auc_score(test_y, y_hat_test), s.cv_score_mean, delta=0.05)
         # Test score greater than test_cv score because first of all I am not making any fit, so the cv out of sample
-        #  set is completely out of sample, and for the test set I am using all the training data, what produces a
+        # set is completely out of sample, and for the test set I am using all the training data, what produces a
         # harder training
+        self.assertIn("test_predictor", dir(s), "Atribute 'test_predictor' not found in the Stacker object")
+        self.assertTrue(type(s.test_predictor) != None)
+
+    def test_consistencies(self):
+        """
+        In this test, let's check if the predictors are bad generated if we instantiate the class and try to build 2
+        different predictors using 2 different models. We are going to check also if the indices of the predictors
+        generated are aligned. We also will assert that the correlation between predictions is high.
+        """
+        from sklearn.ensemble import RandomForestClassifier
+
+        data = make_hastie_10_2(2000)
+        index_randperm = np.random.permutation(range(2000))
+        data = [data[0][index_randperm, :], data[1][index_randperm]]
+        out_of_sample = [data[0][1000:2000, :], data[1][1000:2000]]
+        data = [data[0][0:1000, :], data[1][0:1000]]
+        test_X, test_y = out_of_sample[0], out_of_sample[1]
+        X, y = data[0], data[1]
+        id = np.random.permutation(range(len(y)))  # We random permutate it to make the problem harder.
+        # I am very worried to mess the indices...
+        test_id = np.random.permutation(range(len(test_y)))
+        self.assertEqual(len(X), len(y),
+                         "Error loading hastie data with sklearn. Train/test data with different sizes.")
+        s = Stacker(train_X=X, train_y=y, train_id=id, stratify=False)
+
+        model = RandomForestClassifier(n_estimators=100, random_state=655321)
+        y_hat_training = s.generate_training_metapredictor(model=model)
+        y_hat_test = s.generate_test_metapredictor(test_X, test_id)
+
+        # Regenerate the predictors to check if there are differences
+        self.assertTrue(y_hat_training.equals(s.generate_training_metapredictor(model=model)))
+        self.assertTrue(y_hat_test.equals(s.generate_test_metapredictor(test_X, test_id)))
+
+        model = RandomForestClassifier(n_estimators=100, random_state=112358)
+        y_hat_training_2 = s.generate_training_metapredictor(model=model)
+        y_hat_test_2 = s.generate_test_metapredictor(test_X, test_id)
+
+        # Regenerate the predictors to check if there are differences
+        self.assertTrue(y_hat_training_2.equals(s.generate_training_metapredictor(model=model)))
+        self.assertTrue(y_hat_test_2.equals(s.generate_test_metapredictor(test_X, test_id)))
+
+        # Predictors generated are different (obvious but necessary)
+        self.assertFalse(y_hat_training.equals(y_hat_training_2))
+        self.assertFalse(y_hat_test.equals(y_hat_test_2))
+
+        # Check the index alignments
+        self.assertEqual(y_hat_test.index.tolist(), test_id.tolist())
+        self.assertEqual(y_hat_training.index.tolist(), id.tolist())
+        self.assertEqual(y_hat_test.index.tolist(), y_hat_test_2.index.tolist())
+        self.assertEqual(y_hat_training.index.tolist(), y_hat_training_2.index.tolist())
+        self.assertNotEqual(y_hat_test.index.tolist(), y_hat_training_2.index.tolist())
+        self.assertNotEqual(y_hat_training.index.tolist(), y_hat_test_2.index.tolist())
+
+        # Check the results
+        self.assertAlmostEqual(roc_auc_score(y, y_hat_training), 0.9, delta=0.05)
+        self.assertAlmostEqual(roc_auc_score(test_y, y_hat_test), 0.9, delta=0.05)
+        self.assertAlmostEqual(roc_auc_score(y, y_hat_training_2), 0.9, delta=0.05)
+        self.assertAlmostEqual(roc_auc_score(test_y, y_hat_test_2), 0.9, delta=0.05)
+
+        self.assertFalse(y_hat_training.equals(y_hat_training_2))
+        self.assertFalse(y_hat_test.equals(y_hat_test_2))
+        # The correlation between both model predictions has to be high because we are training the same model but
+        # with different seed
+        self.assertGreater(sp.stats.stats.pearsonr(y_hat_training, y_hat_training_2)[0][0], 0.9)
+        self.assertGreater(sp.stats.stats.pearsonr(y_hat_training, y_hat_training_2)[0][0], 0.9)
+
+    def test_file_storing(self):
+        from sklearn.ensemble import RandomForestClassifier
+
+        data = make_hastie_10_2(2000)
+        index_randperm = np.random.permutation(range(2000))
+        data = [data[0][index_randperm, :], data[1][index_randperm]]
+        out_of_sample = [data[0][1000:2000, :], data[1][1000:2000]]
+        data = [data[0][0:1000, :], data[1][0:1000]]
+        test_X, test_y = out_of_sample[0], out_of_sample[1]
+        X, y = data[0], data[1]
+        id = np.random.permutation(range(len(y)))  # We random permutate it to make the problem harder.
+        # I am very worried to mess the indices...
+        test_id = np.random.permutation(range(len(test_y)))
+        self.assertEqual(len(X), len(y),
+                         "Error loading hastie data with sklearn. Train/test data with different sizes.")
+        s = Stacker(train_X=X, train_y=y, train_id=id, stratify=False)
+
+        model = RandomForestClassifier(n_estimators=100, random_state=655321)
+        y_hat_training = s.generate_training_metapredictor(model=model)
+        y_hat_test = s.generate_test_metapredictor(test_X, test_id)
+
+        s.save_files(alias="demo_predictor", folder="../demo")
+        pathTraining = "../demo/demo_predictor_training_metapredictor.csv"
+        pathTest = "../demo/demo_predictor_test_metapredictor.csv"
+        self.assertTrue(os.path.exists(pathTraining))
+        self.assertTrue(os.path.exists(pathTest))
+        dfTrain = pd.read_csv(pathTraining, sep=",", encoding="utf-8", index_col="id")
+        dfTest = pd.read_csv(pathTest, sep=",", encoding="utf-8", index_col="id")
+        self.assertTrue(len(dfTrain.columns) == 1)
+        self.assertTrue(len(dfTest.columns) == 1)
+        self.assertEqual(dfTrain.index.tolist(), id.tolist())
+        self.assertEqual(dfTest.index.tolist(), test_id.tolist())
+        self.assertIn("target", dfTrain.columns)
+        self.assertIn("target", dfTest.columns)
+        os.remove(pathTraining)
+        os.remove(pathTest)
 
 
     def test_seed_matching(self):
